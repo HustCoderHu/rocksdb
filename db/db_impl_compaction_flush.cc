50557f4e0231f9631ccd87363ff52cab4bce312a
Glitter <zzyyyww9595@gmail.com>
2018-12-07 00:20:03 +0800

    2018/12/06 print pid

--- db/db_impl_compaction_flush.cc
+++ db/db_impl_compaction_flush.cc
@@ -2639,7 +2639,7 @@ namespace rocksdb {
                                     compaction_job_stats, job_context->job_id);
 
             mutex_.Unlock();
-            DBG_PRINT("Compaction run [%d]");
+            DBG_PRINT("Compaction run");
             compaction_job.Run();
             DBG_PRINT("after compaction run");
             TEST_SYNC_POINT("DBImpl::BackgroundCompaction:NonTrivial:AfterRun");
