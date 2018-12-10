f7b6d832fbd8f3af1ac96706415b8e6ce4d2cf6e
Yiwen <zzyyyww9595@gmail.com>
2018-12-10 22:01:40 +0800

    2018-12-10 DoubleBuffer

--- utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
+++ utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
@@ -160,7 +160,7 @@ void FixedRangeChunkBasedNVMWriteCache::MaybeNeedCompaction() {
                                                                         !tab->IsCompactWorking())) {
             DBG_PRINT("Range [%s] Need Compaction [%f]MB > [%f]MB", tab->prefix().c_str(),
                     range_usage.range_size/10418576.0,
-                    vinfo_->internal_options_->range_size_ / 1048576.0);
+                    tab->max_range_size() * 0.9);
             vinfo_->queue_lock_.Lock();
             tab->SetCompactionPendding(true);
             vinfo_->range_queue_.push_back(tab);
