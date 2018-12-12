00df1d98f72defe14f64f06abc629101b0430201
Yiwen <zzyyyww9595@gmail.com>
2018-12-12 15:39:08 +0800

    2018-12-12 DoubleBuffer

--- utilities/nvm_write_cache/fixed_range_tab.cc
+++ utilities/nvm_write_cache/fixed_range_tab.cc
@@ -506,7 +506,7 @@ void FixedRangeTab::SwitchBuffer(SwitchDirection direction) {
             w_buffer_ = w_buffer_->pair_buf_;
             DBG_PRINT("new wbuffer [%lu] cur [%f]",w_buffer_->offset_,  DecodeFixed64(base_raw_+w_buffer_->buf_size_*w_buffer_->offset_) / 1048576.0);
             w_buffer_->writting_ = true;
-            raw_ = base_raw_ + w_buffer_->buf_size_ * w_buffer_->offset_;
+            raw_ = base_raw_ + w_buffer_->buf_size_ * w_buffer_->offset_ + 2 * sizeof(uint64_t);
             DBG_PRINT("raw switch form [%p ]to [%p]",base_raw_+c_buffer_->buf_size_*c_buffer_->offset_,  raw_);
             cblklist_.swap(wblklist_);
             wblklist_.clear();
