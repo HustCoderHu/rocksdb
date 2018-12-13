b90d74c1b5ab8fc78cad0df7192f4b56c3055330
Glitter <zzyyyww9595@gmail.com>
2018-12-13 23:25:59 +0800

    2018/12/13 修改prefix extractor

--- utilities/nvm_write_cache/prefix_extractor.cc
+++ utilities/nvm_write_cache/prefix_extractor.cc
@@ -22,19 +22,25 @@ namespace rocksdb{
         : prefix_bits_(prefix_len) {}
 
     std::string DBBenchDedicatedExtractor::operator()(const char *input, size_t length) {
-        uint64_t intkey;
-        void* rawkey = static_cast<void*>(&intkey);
+        //uint64_t intkey;
+        /*void* rawkey = static_cast<void*>(&intkey);
         for(int i = 0; i < 8; i++){
             memcpy(static_cast<char *>(rawkey)+(7-i), input+i, 1);
+        }*/
+        unsigned int key_num = 0;
+        for(size_t x = 0;x < 8;++x)
+        {
+            key_num = key_num * 16 * 16 + *(unsigned char*)(input + x);
         }
+
         for(int i = 0; i < (16 - prefix_bits_); i++){
-            intkey /= 10;
+            key_num /= 10;
         }
-        uint16_t len = prefix_bits_;
+        uint16_t len = prefix_bits_ + 1;
         char buf[len];
         for(int i = len-1; i >=0; i--){
-            buf[i] = intkey % 10 + '0';
-            intkey /= 10;
+            buf[i] = key_num % 10 + '0';
+            key_num /= 10;
         }
         return std::string(buf);
     }
