76caf458ac10daba2b5965014c96ebfc9ed1883b
Yiwen <zzyyyww9595@gmail.com>
2018-12-22 19:17:22 +0800

    2018-12-22 compaction triggered by threshold

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -4263,7 +4263,7 @@ private:
                         time,
                         rocksdb::delay_count,
                         rocksdb::compact_count);
-                rocskdb::delay_stat[cur] = delay_count;
+                rocksdb::delay_stat[cur] = delay_count;
                 if(++cur = 5) cur = 0;
                 rocksdb::delay_count = 0;
                 rocksdb::compact_count=0;
