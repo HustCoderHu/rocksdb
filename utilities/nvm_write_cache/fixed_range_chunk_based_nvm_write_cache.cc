7f9ba3c8340e6fe2109a813582dca90605d42317
Glitter <zzyyyww9595@gmail.com>
2019-01-28 19:51:08 +0800

    2019/01/28 debug

--- utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
+++ utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
@@ -442,7 +442,7 @@ void FixedRangeChunkBasedNVMWriteCache::CaculateScore() {
 bool FixedRangeChunkBasedNVMWriteCache::CheckRangeUsage() {
     for(auto range : vinfo_->prefix2range){
         if(range.second->RangeTotalSize() > vinfo_->internal_options_->range_size_ * 2 * RANGE_SIZE_MULTIPLE * 0.8){
-            DBG_PRINT("range [%s][%f] score[%f]", range.second->prefix().c_str(), range.second->RangeTotalSize() / 1048576.0, range.second->RangeTotalSize() > vinfo_->internal_options_->range_size_ * 2 * RANGE_SIZE_MULTIPLE);
+            DBG_PRINT("range [%s][%f] score[%f]", range.second->prefix().c_str(), range.second->RangeTotalSize() / 1048576.0, range.second->RangeTotalSize() / vinfo_->internal_options_->range_size_ * 2.0 * RANGE_SIZE_MULTIPLE);
             return true;
         }
     }
